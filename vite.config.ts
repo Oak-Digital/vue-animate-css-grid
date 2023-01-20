b079451855b9305c5d40b3ab1fdfba029ac28989
Alexander Nortung <alexander.nortung@oakdigital.dk>
2023-01-20 12:15:11 +0100

    Added base files

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()], // to process SFC
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'vue-animate-css-grid',
+      formats: ['es'], // adding 'umd' requires globals set to every external module
+      fileName: (format) => `vue-animate-css-grid.${format}.js`,
+    },
+    rollupOptions: {
+      // external modules won't be bundled into your library
+      external: ['vue', /primevue\/.+/], // not every external has a global
+      output: {
+        // disable warning on src/index.ts using both default and named export
+        exports: 'named',
+        // Provide global variables to use in the UMD build
+        // for externalized deps (not useful if 'umd' is not in lib.formats)
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+    emptyOutDir: false,
+  },
+});
